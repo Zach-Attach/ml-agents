d49ce7810726e5c12c87e3da1191d021d97db64f
Zach-Attach <zachlaborde93@gmail.com>
2024-10-29 15:19:06 -0400

    Added note for when this was first forked

--- ml-agents-envs/mlagents_envs/__init__.py
+++ ml-agents-envs/mlagents_envs/__init__.py
@@ -1,4 +1,5 @@
 # Version of the library that will be used to upload to pypi
+#This was forked Oct 29, 2024
 __version__ = "1.2.0.dev0"
 
 # Git tag that will be checked to determine whether to trigger upload to pypi
