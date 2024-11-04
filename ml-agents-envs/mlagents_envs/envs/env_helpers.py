9f59f97be4bbf5ac74d664bec4ea072bee3e8b30
Zach-Attach <zachlaborde93@gmail.com>
2024-11-04 12:36:19 -0500

    Added visual obs fix for env_helpers for zoo

--- ml-agents-envs/mlagents_envs/envs/env_helpers.py
+++ ml-agents-envs/mlagents_envs/envs/env_helpers.py
@@ -1,15 +1,17 @@
+import numpy as np
 from urllib.parse import urlparse, parse_qs
-
+from pettingzoo.utils.env import AgentID, ObsType, ActionType
+from mlagents_envs.base_env import DecisionSteps, TerminalSteps
 
 def _behavior_to_agent_id(behavior_name: str, unique_id: int) -> str:
     return f"{behavior_name}?agent_id={unique_id}"
 
 
-def _agent_id_to_behavior(agent_id: str) -> str:
+def _agent_id_to_behavior(agent_id: AgentID) -> str:
     return agent_id.split("?agent_id=")[0]
 
 
-def _unwrap_batch_steps(batch_steps, behavior_name):
+def _unwrap_batch_steps(batch_steps: tuple[DecisionSteps, TerminalSteps], behavior_name: str, uint8_visual: bool):
     decision_batch, termination_batch = batch_steps
     decision_id = [
         _behavior_to_agent_id(behavior_name, i) for i in decision_batch.agent_id
@@ -26,7 +28,7 @@ def _unwrap_batch_steps(batch_steps, behavior_name):
         obs.update(
             {
                 agent_id: {
-                    "observation": [batch_obs[i] for batch_obs in decision_batch.obs],
+                    "observation": [_preprocess_single(batch_obs[i], uint8_visual) for batch_obs in decision_batch.obs],
                     "action_mask": [mask[i] for mask in decision_batch.action_mask],
                 }
                 for i, agent_id in enumerate(decision_id)
@@ -35,7 +37,7 @@ def _unwrap_batch_steps(batch_steps, behavior_name):
     else:
         obs.update(
             {
-                agent_id: [batch_obs[i] for batch_obs in decision_batch.obs]
+                agent_id: [_preprocess_single(batch_obs[i], uint8_visual) for batch_obs in decision_batch.obs]
                 for i, agent_id in enumerate(decision_id)
             }
         )
@@ -65,7 +67,12 @@ def _unwrap_batch_steps(batch_steps, behavior_name):
     id_map = {agent_id: i for i, agent_id in enumerate(decision_id)}
     return agents, obs, terminations, rewards, cumulative_rewards, infos, id_map
 
-
+def _preprocess_single(single_visual_obs: np.ndarray, uint8_visual: bool) -> np.ndarray:
+    if uint8_visual:
+        return (255.0 * single_visual_obs).astype(np.uint8)
+    else:
+        return single_visual_obs
+        
 def _parse_behavior(full_behavior):
     parsed = urlparse(full_behavior)
     name = parsed.path
